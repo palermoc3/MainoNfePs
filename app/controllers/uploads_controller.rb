52e6776c8ebd4849d75c2757a84cc7a3f6376442
Pedro <pedropalermo97@gmail.com>
2024-08-18 16:36:45 -0300

    Task-1/Upload of files method created

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,70 +1,32 @@
 class UploadsController < ApplicationController
-  before_action :set_upload, only: %i[ show edit update destroy ]
-
-  # GET /uploads or /uploads.json
-  def index
-    @uploads = Upload.all
-  end
-
-  # GET /uploads/1 or /uploads/1.json
-  def show
-  end
+  before_action :set_upload, only: %i[show]
 
   # GET /uploads/new
   def new
     @upload = Upload.new
   end
 
-  # GET /uploads/1/edit
-  def edit
-  end
-
-  # POST /uploads or /uploads.json
+  # POST /uploads
   def create
     @upload = Upload.new(upload_params)
-
-    respond_to do |format|
-      if @upload.save
-        format.html { redirect_to upload_url(@upload), notice: "Upload was successfully created." }
-        format.json { render :show, status: :created, location: @upload }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @upload.errors, status: :unprocessable_entity }
-      end
+    if @upload.save
+      redirect_to @upload, notice: 'Arquivo XML foi carregado com sucesso.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /uploads/1 or /uploads/1.json
-  def update
-    respond_to do |format|
-      if @upload.update(upload_params)
-        format.html { redirect_to upload_url(@upload), notice: "Upload was successfully updated." }
-        format.json { render :show, status: :ok, location: @upload }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @upload.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /uploads/1 or /uploads/1.json
-  def destroy
-    @upload.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to uploads_url, notice: "Upload was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  # GET /uploads/:id
+  def show
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_upload
-      @upload = Upload.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def upload_params
-      params.require(:upload).permit(:xml_file)
-    end
+  def set_upload
+    @upload = Upload.find(params[:id])
+  end
+
+  def upload_params
+    params.require(:upload).permit(:xml_file)
+  end
 end
